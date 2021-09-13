de7c0d5e5ca7e8c3f692ca1ea0a50e30e24521a7
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-09-13 19:15:22 -0400

    head - assert

--- head.js
+++ head.js
@@ -1,3 +1,9 @@
+const head = function(arr){
+  return arr[0];
+}
+console.log(head([5,4,3]))
+
+
 // Function Implementation
 
 const assertEqual = function(actual, expected) {
@@ -7,10 +13,13 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
   }
 };
-
 // test code
 
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1,1);
 assertEqual('Ahmed', 'Ahmed');
 assertEqual(1,12);
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
