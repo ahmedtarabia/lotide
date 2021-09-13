6ea0904c564955c98ca6711068d802c4d786f1af
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-09-13 18:33:13 -0400

    assert

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Function Implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// test code
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1,1);
+assertEqual('Ahmed', 'Ahmed');
+assertEqual(1,12);
